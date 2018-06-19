2bc6db716a5d70ea6cb0711bfa591667d53503f2
Charles <charlesalberthunt@gmail.com>
2018-06-19 01:02:50 -0500

    remove key

--- rangetreemap/src/com/charlesahunt/TreeRangeMap.scala
+++ rangetreemap/src/com/charlesahunt/TreeRangeMap.scala
@@ -12,48 +12,30 @@ class TreeRangeMap[K, V] {
 
   val rangeMap = new mutable.TreeMap[K, RangeEntry[K, V]]
 
-  def asDescendingMapOfRanges(): Map[Range[K], V] = {
+  def asDescendingMapOfRanges(): Map[Range[K], V] = {}
 
-  }
-
-  def asMapOfRanges(): Map[Range[K], V] = {
-
-  }
+  def asMapOfRanges(): Map[Range[K], V] = {}
 
   def clear(): Unit = rangeMap.clear
 
-  def create(): TreeRangeMap[K, V] = {
+  def get(key: K): V = {}
 
-  }
+  def put(range: Range[K], value: V): Unit = {}
 
-  def get(key: K): V = {
+  def putAll(rangeMap: TreeRangeMap[K, V]): Unit = {}
 
-  }
+  def putCoalescing(range: Range[K] , value: V): Unit = {}
 
-  def put(range: Range[K], value: V): Unit = {
+  def remove(rangeToRemove: Range[K]): Unit = rangeMap.remove(rangeToRemove.lower)
 
-  }
+  def span(): Range[K] = {}
 
-  def putAll(rangeMap: TreeRangeMap[K, V]): Unit = {
+  def subRangeMap(subRange: Range[K]): TreeRangeMap[K,V] = {}
 
-  }
+}
 
-  def putCoalescing(range: Range[K] , value: V): Unit = {
-
-  }
-
-  def remove(rangeToRemove: Range[K]): Unit = {
-
-  }
-
-  def span(): Range[K] = {
-
-  }
-
-  def subRangeMap(subRange: Range[K]): TreeRangeMap[K,V] = {
-
-  }
-
+object TreeRangeMap {
+  def apply[K, V]() = new TreeRangeMap[K, V]
 }
 
 case class Range[K](lower: K, upper: K)
